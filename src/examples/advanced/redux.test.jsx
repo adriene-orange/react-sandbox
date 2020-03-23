61aa8302f12f8830fa0a11442d14e08cccd9f4bc
Adriene <adriene_n_cranny@homedepot.com>
2020-03-23 12:04:54 -0700

    add redux example

--- src/examples/advanced/redux.test.jsx
+++ src/examples/advanced/redux.test.jsx
@@ -2,32 +2,51 @@ import React from 'react';
 import {
   render, fireEvent, waitFor,
 } from '@testing-library/react';
-import App from './app';
-import runMockServer from './mockServer';
+import faker from 'faker';
+import App from '../../mirageJS/app';
+import * as ToDoServices from '../../mirageJS/toDoList/toDoServices';
+
+jest.mock('../../mirageJS/toDoList/toDoServices');
+
+const mockToDoFactory = (id, uiStatus = 'LOADED', status = 'To do') => ({
+  id,
+  taskDetails: {
+    value: faker.lorem.word(),
+    uiStatus,
+    status,
+  },
+});
 
 describe('ToDo List App', () => {
-  let server;
-  beforeEach(() => {
-    server = runMockServer({ environment: 'test' });
-  });
-  afterEach(() => {
-    server.shutdown();
+  afterAll(() => {
+    jest.resetModules();
   });
   const FIRST_RENDER = 'an empty input and initial todo tasks are displayed';
   const ADDING_TODO = 'renders a todo card after adding a new task';
+
   test(FIRST_RENDER, async () => {
-    server.createList('todo', 3);
+    // mock out our api requests
+    ToDoServices.fetchAllTodosService.mockResolvedValue({
+      todos: [
+        mockToDoFactory(1),
+        mockToDoFactory(2),
+        mockToDoFactory(3),
+      ],
+    });
     const {
       getAllByTestId,
-      getByPlaceholderText,
     } = render(React.createElement(App));
     await waitFor(() => expect(getAllByTestId('todo-cards')).toHaveLength(3));
-    const addNewTaskInput = getByPlaceholderText('Add a task!');
-    fireEvent.change(addNewTaskInput, { target: { value: 'Write tests' } });
-    fireEvent.submit(addNewTaskInput);
   });
   test(ADDING_TODO, async () => {
-    server.createList('todo', 3);
+    ToDoServices.fetchAllTodosService.mockResolvedValue({
+      todos: [
+        mockToDoFactory(1),
+        mockToDoFactory(2),
+        mockToDoFactory(3),
+      ],
+    });
+    ToDoServices.addToDoService.mockResolvedValue({ body: {} });
     const {
       getByTestId,
       getAllByTestId,
